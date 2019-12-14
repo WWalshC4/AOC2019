286e7bd1b988cfe0b84d0e71807593f4597d23bc
William Walsh <wwalsh@control4.com>
2019-12-14 14:33:38 -0500

    day9 complete

--- Lua/day9b.lua
+++ Lua/day9b.lua
@@ -307,4 +307,4 @@ end
 --runTape ({109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99}) -- each of these numbers
 --runTape ({1102,34915192,34915192,7,4,7,99,0})	--1219070632396864
 --runTape ({104,1125899906842624,99})	--1125899906842624
-runTape (inputTape) -- input:1, output: 3241900951
+runTape (inputTape) -- input:2, output: 83089
